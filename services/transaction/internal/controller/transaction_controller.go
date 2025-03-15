6484e45c438dabe06cd08a107fa315a28d487ebb
myacey <grg1nev@gmail.com>
2025-03-15 13:29:49 +0300

    added README.md

--- services/transaction/internal/controller/transaction_controller.go
+++ services/transaction/internal/controller/transaction_controller.go
@@ -46,31 +46,25 @@ func (h *Controller) SearchEntriesForUser(c *gin.Context) {
 	// 	h.JSONError(c, err)
 	// }
 
-	h.lg.Info("1")
 	offset, err := strconv.Atoi(c.Query("offset"))
 	if err != nil {
-		h.lg.Info("2")
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid offset"})
 		return
 	}
 	limit, err := strconv.Atoi(c.Query("limit"))
 	if err != nil {
-		h.lg.Info("3")
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid limit"})
 		return
 	}
-	h.lg.Info("4")
 
 	username := c.GetHeader("X-User-Username")
 	if username == "" {
 		h.JSONError(c, cstmerr.ErrInvalidToken)
 		return
 	}
-	h.lg.Info("5")
 
 	trxs, err := h.srv.SearchEntriesForUser(c, username, offset, limit)
 	if err != nil {
-		h.lg.Info("6")
 		h.JSONError(c, err)
 		return
 	}
